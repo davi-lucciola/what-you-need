01cfb39e0d042612df4fe1b7445eac0bbf9a4b09
Davi Lucciola <davilucciola14@gmail.com>
2026-06-25 01:13:02 -0300

    feat: setup langsmith and langgraph studio

--- app/config.py
+++ app/config.py
@@ -1,7 +1,5 @@
-from pydantic import Field
 from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    GOOGLE_API_KEY: str = Field()
-    AGENT_CHAT_MODEL: str = Field('google_genai:gemini-2.5-flash')
+    AGENT_CHAT_MODEL: str
